d2a8a0f71aa68399af1db99cbba2a839e6232c3a
Andrzej Gac <gac.andrzej@gmail.com>
2025-10-16 17:46:53 +0200

    add sealed

---
+++ src/sealed/Komputer.kt
@@ -0,0 +1,53 @@
+package sealed
+
+import komputery.Komputer
+
+sealed class Komputer(
+    var producent: String = "",
+    var model: String = "",
+    var rokProdukcji: Int = 0
+) {
+    override fun toString(): String =
+        "Komputer(producent='$producent', model='$model', rokProdukcji=$rokProdukcji)"
+
+    open fun wlacz() {
+        println("Komputer się uruchamia ...")
+    }
+}
+
+class Laptop(
+    producent: String = "",
+    model: String = "",
+    rokProdukcji: Int = 0,
+    var waga: Double = 0.0,
+    var iloscPortowUSB: Int = 0,
+    var czasPracyNaBaterii: Float = 0.0f
+) : Komputer(producent, model, rokProdukcji) {
+
+    override fun toString(): String {
+        return "Laptop(producent='$producent', model='$model', rokProdukcji=$rokProdukcji, " +
+                "waga=$waga, iloscPortowUSB=$iloscPortowUSB, czasPracyNaBaterii=$czasPracyNaBaterii)"
+    }
+
+    override fun wlacz() {
+        println("Laptop się uruchamia ...")
+    }
+}
+
+class Smartfon(
+    producent: String = "",
+    model: String = "",
+    rokProdukcji: Int = 0,
+    var systemOperacyjny: String = "",
+    var iloscPamieci: Int = 0
+) : Komputer(producent, model, rokProdukcji) {
+
+    override fun toString(): String {
+        return "Smartfon(producent='$producent', model='$model', rokProdukcji=$rokProdukcji, " +
+                "systemOperacyjny='$systemOperacyjny', iloscPamieci=$iloscPamieci)"
+    }
+
+    override fun wlacz() {
+        println("Smartfon się uruchamia ... $systemOperacyjny")
+    }
+}
---
+++ src/sealed/Main.kt
@@ -0,0 +1,57 @@
+package sealed
+
+import komputery.Komputer
+import komputery.Laptop
+import komputery.Smartfon
+
+fun main() {
+    val komputer = Komputer("Asus", "AX25", 2024)
+    // komputer.model = "sdfsd" // właściwość public, ale można zmienić tylko jeśli jest 'var'
+
+    val pc2 = Komputer()
+    // settery - bezpośredni dostęp do właściwości
+    pc2.producent = "HP"
+    pc2.model = "QW23"
+    pc2.rokProdukcji = 2025
+
+    // gettery - bezpośredni dostęp do właściwości
+    println(komputer.toString())
+    println(pc2.toString())
+
+    // test klasy Laptop
+    val laptop = Laptop("Lenovo", "GPX2", 2023, 1.6, 3, 6.5f) // Ctrl + P
+    println(laptop.toString())
+
+    // z konstruktorem bezargumentowym
+    val laptop2 = Laptop()
+    laptop2.producent = "Samsung"
+    laptop2.model = "XCV"
+    laptop2.rokProdukcji = 2026
+    laptop2.waga = 2.2
+    laptop2.iloscPortowUSB = 4
+    laptop2.czasPracyNaBaterii = 10.3f
+
+    // wypisanie, korzystamy z metody toString()
+    println(laptop2.toString())
+
+    // testujemy polimorfizm
+    komputer.wlacz()
+    pc2.wlacz()
+    println(System.lineSeparator())
+    laptop.wlacz()
+    laptop2.wlacz()
+
+    // Smartfon
+    val smartfon = Smartfon("Google", "Pixel 10 Pro", 2025, "Android", 8)
+    val smartfon2 = Smartfon("Apple", "17 Pro", 2025, "Apple IOS", 16)
+    smartfon2.producent = "Apple"
+    smartfon2.model = "17 Pro"
+    smartfon2.rokProdukcji = 2025
+    smartfon2.systemOperacyjny = "ios 17"
+    smartfon2.iloscPamieci = 16
+
+    println(smartfon.toString())
+    println(smartfon2.toString())
+    smartfon.wlacz()
+    smartfon2.wlacz()
+}
